e65988273f96734eadfaac036b00a8f2543466e9
Artur Kuciński <artuyees@gmail.com>
2022-02-03 18:39:58 +0100

    Zadanie 4 z matury z 2012 roku.

---
+++ python-2012/zadanie1.py
@@ -0,0 +1,29 @@
+file = open("tj.txt")
+keys_file = open("klucze1.txt")
+lines = file.readlines()
+keys = keys_file.readlines()
+file.close()
+keys_file.close()
+
+
+lista = []
+for line in lines:
+    encrypted_word=""
+    x = lines.index(line)
+    key = keys[x]
+    key = key.rstrip()
+    line = line.rstrip()
+    index = 0 
+    for letter in line:
+        if index>(len(key)-1):
+            index = 0
+        number = ord(letter)
+        key_letter = key[index]
+        key_of_number = ord(key_letter)-64
+        result = number + key_of_number
+        if result > 90:
+            result = result - 26
+        encrypted_word += chr(result)
+        index+=1
+    lista.append(encrypted_word)
+print(lista)
