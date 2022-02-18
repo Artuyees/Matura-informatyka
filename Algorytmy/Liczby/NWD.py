8b390aa565448067445260313059b5f829b5e6a4
Artur Kucinski <artuyees@gmail.com>
2022-02-18 23:58:44 +0100

    Poprawka na praktykę

--- Algorytmy/Liczby/NWD.py
+++ Algorytmy/Liczby/NWD.py
@@ -1,5 +1,8 @@
 # algorytm wyszukujacy najwiekszy wspolny dzielnik. 
 
+import math
+
+
 def nwd_i(a, b):
     while b:
         a, b = b, a%b
@@ -11,3 +14,10 @@ second = input("wprowadz druga liczbe: ")
 
 first, second = int(first), int(second)
 print(nwd_i(first, second))
+
+
+#Na praktyce nie pisac takiego algorytmu.
+#Wbudowana biblioteka math zawiera funkcje math.gcd(pierwszaCyfra, drugaCyfra).
+
+# Pog
+print(math.gcd(first, second ))
