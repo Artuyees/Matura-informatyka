776761ae6a19d4422ebfba2440dd9960615fa2b2
Artur Kucinski <artuyees@gmail.com>
2022-02-21 22:52:47 +0100

    Naprawiono błąd małych liter

--- Algorytmy/szyfry/cezar.py
+++ Algorytmy/szyfry/cezar.py
@@ -1,5 +1,5 @@
 def szyfruj(word: str, key: int):
-    word_1 = word.strip()
+    word_1 = word.strip().upper()
     word_2 = ''
     for letter in word_1:
         ascii_letter = ord(letter) 
@@ -10,5 +10,23 @@ def szyfruj(word: str, key: int):
         word_2 += chr(ascii_letter)
     return word_2
 
-x = szyfruj("OKOKOK", 20)
-print(x)
+def odszyfruj(word: str, key: int):
+    word_1 = word.strip().upper()
+    word_2 = ''
+    for letter in word_1:
+        ascii_letter = ord(letter) 
+        for i in range(key):
+            ascii_letter-=1
+            if  ascii_letter == 64:
+                ascii_letter += 26
+        word_2 += chr(ascii_letter)
+    return word_2
+
+# KONIEC ALGORYTMU 
+input_word  = input("Wprowadz tekst: ")
+checkbox = input("zaszyfrowac czy odszyfrowac? (1 lub 2): ")
+
+if checkbox == "1":
+    print(f"zaszyfrowany tekst: {szyfruj(input_word, 20)}")
+else: 
+    print(f"odszyfrowany tekst {odszyfruj(input_word, 20)}")
